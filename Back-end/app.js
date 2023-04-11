0360f45d7a81411c75f12418c485749f6f68fe4b
Tal Mogendorff <71597455+TalMogendorff@users.noreply.github.com>
2023-04-11 12:49:53 +0300

    Update app.js

--- Back-end/app.js
+++ Back-end/app.js
@@ -7,7 +7,7 @@ const users = require("./routes/user");
 const auth = require("./routes/auth");
 const projects = require('./routes/emrProjects')
 mongoose
-  .connect("mongodb://localhost/Sheba_api", {
+  .connect("mongodb://localhost/doctor_api", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -22,9 +22,9 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use("/sheba/users", users);
-app.use("/sheba/auth", auth);
-app.use("/sheba/projects", projects);
+app.use("/doctor/users", users);
+app.use("/doctor/auth", auth);
+app.use("/doctor/projects", projects);
 
 const port = 8181;
 http.listen(port, () => console.log(`Listening on port ${port}...`));
